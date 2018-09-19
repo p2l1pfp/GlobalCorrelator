8f41f68c92b2660ad6d61b0ca538ef59f2b4dc05
Alexx Perloff <Alexx.Perloff@Colorado.edu>
2018-09-19 12:40:44 -0600

    Extend the stuct example to test for more advanced structs (i.e. member functions).

--- pulsar_devkit/simple_examples/HLSIPs/src/simple_algo_struct.h
+++ pulsar_devkit/simple_examples/HLSIPs/src/simple_algo_struct.h
@@ -3,23 +3,40 @@
 
 #include "ap_int.h"
 
+typedef ap_int<32> value_t;
+
 //#define __EXTENDED__
+#define __MEMBERS__
 #ifndef __EXTENDED__
 	struct simple_struct {
-	    ap_int<32> inA;
-	    ap_int<32> inB;
-	    
+	    value_t inA;
+	    value_t inB;
+	    #ifdef __MEMBERS__
+	    void clear() {inA = 0; inB = 0;}
+	    value_t sum() {return inA+inB;}
+	    #endif
 	};
 #else
 	struct simple_struct {
-	    ap_int<32> inA;
-	    ap_int<32> inB;
-	    ap_int<32> inC;
-	    
+	    value_t inA;
+	    value_t inB;
+	    value_t inC;
+	    #ifdef __MEMBERS__
+	    void clear() {inA = 0; inB = 0;}
+	    value_t sum() {return inA+inB;}
+	    #endif
 	};
 #endif
+#ifndef __MEMBERS__
+inline void clear(simple_struct & structA) {
+	structA.inA = 0; structA.inB = 0;
+	#ifdef __EXTENDED__
+	structA.inC = 0;
+	#endif
+}
+#endif
 
-void simple_algo_struct_ref( simple_struct structA, ap_int<32> &outA );
-void simple_algo_struct_hw( simple_struct structA, ap_int<32> &outA );
+void simple_algo_struct_ref( simple_struct structA, value_t &outA );
+void simple_algo_struct_hw( simple_struct structA, value_t &outA );
 
 #endif
